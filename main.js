23cc9a8bc76abd9371591f2c23c2850c303d5950
nathaliaflavia232 <nathaliaflavia232@gmail.com>
2024-10-11 11:10:19 +0000

    ai ai

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// Add JS here
+{
+    const numero1 = Number(document.querySelector("#numero-1").value)
+    const numero2 = Number(document.querySelector("#numero-2").value)
+    const numero3 = Number(document.querySelector("#numero-3").value)
+
+    const paragrafo = document.querySelector(".resultado")
+
+    paragrafo.innerHTML = `Resultado: ${numero1 + numero2}`
+}
+
+const button = document.querySelector("button")
+button.addEventListener('click', somar)
